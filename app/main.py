a24a42aa291b091dc305618852b6bb21bd3fb022
zwirok-git <oleksijprokopenko@gmail.com>
2026-04-03 00:07:32 +0300

    implemented classes

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.health = health
+        self.name = name
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, herbivore: Herbivore) -> None:
+        if not herbivore.hidden and isinstance(herbivore, Herbivore):
+            herbivore.health -= 50
+
+            if herbivore.health <= 0:
+                Animal.alive.remove(herbivore)
